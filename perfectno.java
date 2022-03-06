757bc6f80df8f124bfe77b03a5ee89d96abe1adc
mandavatarun <100502836+mandavatarun@users.noreply.github.com>
2022-03-06 11:05:59 +0530

    Add files via upload

---
+++ perfectno.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class perfectno {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n= s.nextInt();
+		
+	}
+}
